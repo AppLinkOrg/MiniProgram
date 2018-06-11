84ac6555d6564ed878750a1db0cd2e19c011754e
alucard263096 <alucard263096@126.com>
2018-06-11 20:27:12 +0800

    commit

--- source/pages/productlist/productlist.js
+++ source/pages/productlist/productlist.js
@@ -1,7 +1,13 @@
 // pages/content/content.js
-import { AppBase } from "../../appbase";
-import { ApiConfig } from "../../apis/apiconfig";
-import { InstApi } from "../../apis/inst.api.js";
+import {
+  AppBase
+} from "../../appbase";
+import {
+  ApiConfig
+} from "../../apis/apiconfig";
+import {
+  InstApi
+} from "../../apis/inst.api.js";
 
 class Content extends AppBase {
   constructor() {
@@ -12,28 +18,35 @@ class Content extends AppBase {
     //options.id=5;
     super.onLoad(options);
 
-    this.Base.setMyData({ productcatlist: [], productlist: [], selectcat:0 });
+    this.Base.setMyData({
+      productcatlist: [],
+      productlist: [],
+      selectcat: 0
+    });
   }
   onMyShow() {
     var that = this;
     var instapi = new InstApi();
-    var productcatlist = this.Base.getMyData().productcatlist;
-    if(productcatlist.length==0){
-      
-      instapi.productcatlist({}, (productcatlist) => {
-        that.Base.setMyData({ productcatlist: productcatlist
-        , selectcat: productcatlist[0].id
-          , productlist: productcatlist[0].productlist
-         });
+
+    instapi.productcatlist({}, (productcatlist) => {
+      var plist = this.Base.getMyData().productcatlist;
+      if (plist.length == 0) {
+        that.Base.setMyData({
+          selectcat: productcatlist[0].id,
+          productlist: productcatlist[0].productlist
         });
-    }
+      }
+      that.Base.setMyData({
+        productcatlist: productcatlist
+      });
+    });
   }
-  changeCat(e){
-    var index=e.currentTarget.id;
+  changeCat(e) {
+    var index = e.currentTarget.id;
     var productcatlist = this.Base.getMyData().productcatlist;
     this.Base.setMyData({
-      selectcat: productcatlist[index].id
-      , productlist: productcatlist[index].productlist
+      selectcat: productcatlist[index].id,
+      productlist: productcatlist[index].productlist
     });
   }
 }
